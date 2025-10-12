9ce59415e68ab59f6af8d6f9c823f574628e4409
GuptaJuhi14 <ms.gjuhi14@gmail.com>
2025-10-12 18:21:58 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,43 @@
+let score = "33"
+
+// const {score} = req.body // here it is not sure what is the type of score
+
+console.log(typeof score)
+console.log(typeof(score)) //as a method
+
+// Operation is to be performed on numbers
+
+let valueInNumber = Number(score) //conversion of string to number datatype
+
+console.log(typeof valueInNumber)
+
+let num = "33abc"
+
+let valueInNumber2 = Number(num)
+
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2) // output-> NaN /which is not a number
+
+// on conversion into number
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// numm => 0
+// undefined => undefined
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // output=> true
+
+//on conversion into boolean
+// 1=> true; 0 => false
+// "" => false
+// "gupta" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.group(typeof stringNumber)
+console.log(stringNumber)
