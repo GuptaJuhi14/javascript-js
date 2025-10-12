f42fb5388811ecfeb71ec60022a8d71f3c1e4912
GuptaJuhi14 <ms.gjuhi14@gmail.com>
2025-10-12 13:50:33 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version(JS)
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3)//code readability should be high
+
+    let name = "Gupta" //double quotes so string
+    let age = 23 //number
+    let isLoggedIn = true //boolean
+
+    // number => 2 to power 53
+    // bigInt => trading and stock market where large numbers exist
+    // string => "" //'' can also be used
+    // boolean => true/false
+    // null => standalone value // representation of empty value //declared variable is not undefined it is empty
+    // undefined => //value is not assigned yet/ when u dont define value that variable is declared but value is not assigned to it.
+    // symbol => unique//implemented mostly in react when we make a lot of components and have to identify uniqueness of components
+    
+    // OBJECT
+
+    console.log(typeof "Gupta"); //output -> string
+    console.log(typeof age) // output -> number
+    console.log(typeof null) // output -> object
+    console.log(typeof undefined) //output -> undefined
