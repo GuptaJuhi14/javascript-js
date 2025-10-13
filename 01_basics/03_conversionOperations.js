3aad988ca2a0c35b81eddb9f120c69875a723a5e
GuptaJuhi14 <ms.gjuhi14@gmail.com>
2025-10-13 09:38:09 +0000

    string to number conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -41,3 +41,44 @@ let stringNumber = String(someNumber)
 
 console.group(typeof stringNumber)
 console.log(stringNumber)
+
+// ***** Operations ***** //
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Gupta"
+let str3 = str1 +str2
+console.log(str3)
+
+console.log("1"+2) // output => 12
+console.log(1+"2") // output => 12
+console.log("1"+2+2) // ouutput => 122 / first element is string so rest all will be treated as string
+console.log(1+2+"2") // output => 32  
+
+// ** Tricky conversion **
+
+console.log(true) // output => true
+console.log(+true) // output => 1
+// console.log(true +); //error
+console.log(+"");// output => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);// output => 101
+++gameCounter
+console.log(gameCounter); // output =>101
+
