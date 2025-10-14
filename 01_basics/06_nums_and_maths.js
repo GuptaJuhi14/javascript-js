4176dff2aa098f43a8dd0369d94bb10ffa2502fb
GuptaJuhi14 <ms.gjuhi14@gmail.com>
2025-10-14 20:15:48 +0000

    numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 400 // JS automatically detects that the type is Number
+console.log(score); // output => 400
+
+const balance = new Number(100) // type is specifically defined
+console.log(balance); // output => [Number: 100]
+
+console.log(balance.toString()); // output => 100
+console.log(balance.toString().length) // output => 3
+console.log(balance.toFixed(2)); // output => 100.00
+console.log(balance.toFixed(1)); // output => 100.0
+
+const otherNumber = 23.8938
+
+console.log(otherNumber.toPrecision(3)); // output => 23.9
+console.log(otherNumber.toPrecision(2)); // output => 24
+console.log(otherNumber.toPrecision(1)); // output => 2e+1
+
+const hundreds = 1000000
+console.log(hundreds.toLocalString);
